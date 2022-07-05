7985406215948514452c067e81aa77e5d95f8e19
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-07-05 13:19:40 +0900

    Feature : Create Rendering Views

--- mall/views.py
+++ mall/views.py
@@ -1,5 +1,31 @@
+
 from django.shortcuts import render
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch('http://localhost:9200')
+
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+
+    _from = request.GET.get("page")
+    size = 20
+
+    if _from == "first":
+        _from = 0
+    elif _from == "last":
+        _from = int(459 / size) * size
+
+    index = "products"
+    body = { 
+            "from" : _from,
+            "size" : size
+        }
+    
+    res = es.search(index=index, body=body)
+
+    product_list = [res.get("_source") for res in res.get("hits").get("hits")]
+    page_list = [int(_from) + i for i in range(0, 10)]
+
+    return render(request, "index.html", {"product_list": product_list, "page_list":page_list})
+
